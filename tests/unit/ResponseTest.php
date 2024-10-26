5e3615a74e054211b19bc1a4d18cd9b51316fe21
Andy Scherzinger <info@andy-scherzinger.de>
2024-10-26 15:06:12 +0200

    docs(spdx): Add SPDX header
    Signed-off-by: Andy Scherzinger <info@andy-scherzinger.de>

--- tests/unit/ResponseTest.php
+++ tests/unit/ResponseTest.php
@@ -1,6 +1,7 @@
 <?php
 /**
- * @license MIT <http://opensource.org/licenses/MIT>
+ * SPDX-FileCopyrightText: 2018 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: MIT
  */
 
 namespace Tests;
